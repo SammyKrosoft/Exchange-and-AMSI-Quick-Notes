69484c16d913800c5d9325c082930d6c578d40bf
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2021-08-11 09:58:39 -0400

    Update EnableDisableAMSIUsingOverrides.ps1

--- EnableDisableAMSIUsingOverrides.ps1
+++ EnableDisableAMSIUsingOverrides.ps1
@@ -15,8 +15,8 @@ Restart-Service -Name W3SVC, WAS -Force
 # Check if the server performance is back to normal (CPU, RAM, MAPIHTTP counters,...)
 
 # Run Exchange Management Shell and run these three commands to remove the setting override:
-1. Remove-SettingOverride -Name “DisablingAMSIScan” -Component Cafe -Section HttpRequestFiltering -Parameters (“Enabled=False”) -Reason “Testing”
-2. Get-ExchangeDiagnosticInfo -Process Microsoft.Exchange.Directory.TopologyService -Component VariantConfiguration -Argument Refresh
-3. Restart-Service -Name W3SVC, WAS -Force
+Remove-SettingOverride -Name “DisablingAMSIScan” -Component Cafe -Section HttpRequestFiltering -Parameters (“Enabled=False”) -Reason “Testing”
+Get-ExchangeDiagnosticInfo -Process Microsoft.Exchange.Directory.TopologyService -Component VariantConfiguration -Argument Refresh
+Restart-Service -Name W3SVC, WAS -Force
 
 
